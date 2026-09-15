3b0ce4e3435684a53914ad5580a8d97b890162cb
Sabrina_wosniak <sabrinasouzawozniak@gmail.com>
2026-09-15 11:19:57 -0300

    feat(produtos): adiciona model e migrations

--- produtos/models.py
+++ produtos/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+
+class Produto(models.Model):
+    nome = models.CharField(max_length=100)
+    preco = models.DecimalField(max_digits=8, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.nome} - R$ {self.preco}"
